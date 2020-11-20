7e3c4c1b7d0e5d9bdc068914f7155e9fdc5e010e
ArmandAlbert <46563594+ArmandAlbert@users.noreply.github.com>
2020-11-20 22:33:53 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -9,82 +9,11 @@ import java.util.Scanner;
 
 
 public class choice {
-    public static class Course {
-        public String getCourseName() {
-            return courseName;
-        }
-
-        public int getCourseHour() {
-            return courseHour;
-        }
-
-        public void setCourseHour(int courseHour) {
-            this.courseHour = courseHour;
-        }
-
-        public void setCourseName(String courseName) {
-            this.courseName = courseName;
-        }
-
-        Course() {
-        }
-
-        Course(String cName, int cHour) {
-            this.setCourseName(cName);
-            this.setCourseHour(cHour);
-        }
-
-        private String courseName;
-        private int courseHour;
-
-    }
-
-    public static class Student {
-        private String name;
-        private int grade;
-        private Course[] courses;
-
-        public int getHours() {
-            int sum = 0;
-            for (int i = 0; i < this.courses.length; i++) {
-                sum += courses[i].getCourseHour();
-            }
-            return sum;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setGrade(int grade) {
-            this.grade = grade;
-            this.courses = new Course[grade];
-        }
-
-        public void setCourses(int i, String cName, int hour) {
-            this.courses[i] = new Course();
-            this.courses[i].setCourseName(cName);
-            this.courses[i].setCourseHour(hour);
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getGrade() {
-            return grade;
-        }
-
-        public Course[] getCourses() {
-            return courses;
-        }
-
-        public void showMessage() throws IOException {
-            Printer.print(String.format("%s %d年级\n选修的课程包括:\n", name, grade));
-            for (int i = 0; i < courses.length; i++)
-                Printer.print(String.format("(%d)%s\t%d分\n", i + 1, courses[i].getCourseName(), courses[i].getCourseHour()));
-            Printer.print(String.format("总学分:%d\n", getHours()));
-        }
+        /*
+       
+       Todo
+        
+        */
 
     }
 
